b31a17ce5da0b24c8eb58f53e066cc5ad3cf6f5d
zerai <teclaizerai@gmail.com>
2021-09-19 13:41:05 +0200

    [Tools] update ecs configuration.

--- ecs.php
+++ ecs.php
@@ -18,6 +18,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
+        __DIR__ . '/tests',
     ]);
 
     $parameters->set(
