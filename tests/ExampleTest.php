b982f526568c63ccd2da6c5095f853ba273e9f55
zerai <teclaizerai@gmail.com>
2021-09-19 13:41:53 +0200

    Added placeholder/example test.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace OpenEMR\Modules\Memu\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
